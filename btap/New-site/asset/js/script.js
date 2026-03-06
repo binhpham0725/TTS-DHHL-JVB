004c47e1972ae77148e67b4a8f3f82e74c776369
Pbkh042 <khoartk135@gmail.com>
2026-03-06 17:19:24 +0700

    update btap

--- btap/New-site/asset/js/script.js
+++ btap/New-site/asset/js/script.js
@@ -11,3 +11,15 @@ function toggleMenu(id, btn){
     }
 
 }
+
+const signUpBtn = document.getElementById("signUp");
+const signInBtn = document.getElementById("signIn");
+const container = document.getElementById("container");
+
+signUpBtn.addEventListener("click", () => {
+    container.classList.add("right-panel-active");
+});
+
+signInBtn.addEventListener("click", () => {
+    container.classList.remove("right-panel-active");
+});
